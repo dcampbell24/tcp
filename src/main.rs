83f3727323de3caeef0db6d48ec00b528ed1b3e3
David Campbell <dcampbell24@gmail.com>
2025-12-14 20:16:00 -0500

    Expand the test.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,43 @@
-use std::net::TcpStream;
+use std::{
+    net::{Shutdown, TcpStream},
+    sync::mpsc::channel,
+    thread::{self, sleep},
+    time::Duration,
+};
 
 const ADDRESS: &str = "hnefatafl.org:49152";
 
 fn main() {
-    println!("Connecting to server...");
-    TcpStream::connect(ADDRESS).unwrap();
-    println!("Connected.");
+    let (tx, rx) = channel();
+
+    thread::spawn(move || {
+        loop {
+            let message: &str = rx.recv().unwrap();
+            let message_trim = message.trim();
+
+            if message_trim == "start_tcp" {
+                println!("Connecting to server...");
+                let tcp_stream = TcpStream::connect(ADDRESS).unwrap();
+                println!("Connected.");
+
+                loop {
+                    let message = rx.recv().unwrap();
+                    let message_trim = message.trim();
+
+                    if message_trim == "stop_tcp" {
+                        tcp_stream
+                            .shutdown(Shutdown::Both)
+                            .expect("shutdown call failed");
+
+                        return;
+                    }
+                }
+            }
+        }
+    });
+
+    tx.send("start_tcp").unwrap();
+    tx.send("stop_tcp").unwrap();
+
+    sleep(Duration::from_secs(1));
 }
